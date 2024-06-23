accdf6afdbe4e043e1bfedf2e82e55e8c97f2cc4
David Coomber <47242934+dcoomber@users.noreply.github.com>
2024-06-23 16:41:22 +0200

    Update to latest k6 image; update Playwright; fix issues reported when running test

--- src/k6/example-test.ts
+++ src/k6/example-test.ts
@@ -1,20 +1,28 @@
 import { check } from 'k6';
 import { Options } from 'k6/options';
-import { chromium } from 'k6/experimental/browser';
+import { browser } from 'k6/experimental/browser';
 import { clickCheckboxOnk6 } from '@pages/example-page';
 
-
-export let options: Options = {
+export const options: Options = {
     vus: 1,
-    duration: '10s'
-};
+    scenarios: {
+      perftest: {
+        executor: 'per-vu-iterations',
+        options: {
+          browser: {
+            type: 'chromium',
+          },
+        },
+      },
+    },
+  }
 
 export default async function () {
-    const browser = chromium.launch({
-        headless: true, args: ['no-sandbox']
-    });
-    const context = browser.newContext();
-    const page = context.newPage();
+    // const browser = chromium.launch({
+    //     headless: true, args: ['no-sandbox']
+    // });
+    // const context = browser.newContext();
+    const page = browser.newPage();
     try {
         await clickCheckboxOnk6(page);
         check(page, {
@@ -23,6 +31,6 @@ export default async function () {
         });
     } finally {
         page.close();
-        browser.close();
+        // browser.close();
     }
 };
